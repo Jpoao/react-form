7ad86c8a8790eed0dcabeab32e79e1270bb2a0bd
João Paulo <mg_ses@hotmail.com>
2021-08-15 18:58:39 -0300

    hook values

--- src/pages/CepSearch/index.tsx
+++ src/pages/CepSearch/index.tsx
@@ -1,18 +1,40 @@
+import { useState } from "react";
 import ResultCard from "../../components/ResultCard"
 import "./styles.css"
 
+type FormData = {
+  cep: string;
+}
+
 const CepSearch = () => {
+
+  const [formData, setFormData] = useState<FormData>({
+    cep: ''
+  })
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({...formData, [name]:value});
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
     <div className="cep-search-container">
       <h1 className="text-primary">Busca CEP</h1>
       <div className="container search-container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-container">
             <input
               type="text"
+              name="cep"
+              value={formData.cep}
               className="search-input"
               placeholder="CEP (somente números)"
-              onChange={() => {}}
+              onChange={handleChange}
             />
             <button type="submit" className="btn btn-primary search-button">
               Buscar
